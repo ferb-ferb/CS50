0f9b5739f249768ddae8c8326a64dd330ca3d18c
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2022-07-27 19:19:24 +0000

    Wed, Jul 27, 2022, 7:19 PM +00:00

--- test/tester.c
+++ test/tester.c
@@ -3,10 +3,5 @@
 #include <cs50.h>
 
 int main(void){
-    string name = get_string("Whats your name? \n");
-    for(int i = 0; i < 10 ; i++)
-    {
-        printf("Hello %s " , name);
-    }
-    printf("\n");
+    
 }
