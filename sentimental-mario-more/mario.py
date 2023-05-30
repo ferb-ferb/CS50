786dc109093361f5fd1d9e5281df844bb2a5dcc9
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-30 03:15:46 +0000

    Mon, May 29, 2023, 8:15 PM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,6 +4,6 @@ def main():
         if height >0 and height <9:
             break
     for i in range(height):
-        print(" " * (height-i-1) + "#"*(i+1)  +"  " + "#"*(i+1) + " " * (height-i-1))
+        print(" " * (height-i-1) + "#"*(i+1)  +"  " + "#"*(i+1))
 
 main()
