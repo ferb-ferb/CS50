cb52cb27620ed5e10247b127751225eac5785d37
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-30 13:53:57 +0000

    Tue, May 30, 2023, 6:53 AM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,8 +3,15 @@ from cs50 import get_string
 
 def main():
     text = get_string("Text: ")
+    letters = 0
+    spaces = 0
+    periods = 0
     for c in text:
-        
-
-
+        if c.isalpha():
+            letters += 1
+        elif c == " ":
+            spaces += 1
+        elif c == ".":
+            periods += 1
+    
 main()
