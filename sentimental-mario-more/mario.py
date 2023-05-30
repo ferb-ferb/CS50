822268a362061b2fbe51c1c10c835f35764d0610
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-30 03:18:43 +0000

    Mon, May 29, 2023, 8:18 PM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,10 @@
 def main():
     while True:
-        height = int(input("Height: "))
-        if height >0 and height <9:
-            break
-    for i in range(height):
-        print(" " * (height-i-1) + "#"*(i+1)  +"  " + "#"*(i+1))
+        try:
+            height = int(input("Height: "))
+            if height >0 and height <9:
+                break
+            for i in range(height):
+            print(" " * (height-i-1) + "#"*(i+1)  +"  " + "#"*(i+1))
 
 main()
