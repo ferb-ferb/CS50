c195c04cde4165141ee4f4c94b7a97eb5a04660e
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-30 14:33:28 +0000

    Tue, May 30, 2023, 7:33 AM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,17 +11,18 @@ def main():
             letters += 1
         elif c == " ":
             words += 1
-        elif c in ["." , "?", "!"]:
+        elif c in [".", "?", "!"]:
             periods += 1
     L = letters/words * 100
     S = periods/words * 100
     cl = 0.0588 * L - 0.296 * S - 15.8
     cl = int(round(cl, 0))
-    if cl>0 and cl<17:
+    if cl > 0 and cl < 17:
         print(f"Grade: {cl}")
-    elif cl>16:
+    elif cl > 16:
         print("Grade 16+")
     else:
         print("Before Grade 1")
+
 
 main()
