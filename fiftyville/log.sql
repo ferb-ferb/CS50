e850131baa81464d0eac5b64877abc49871be07c
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-31 23:37:15 +0000

    Wed, May 31, 2023, 4:37 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,4 +6,5 @@ SELECT * FROM interviews WHERE day = 28 AND month = 7 AND year = 2021;
 --FIND license plates that left bakery within 10 min of crime
 SELECT activity,license_plate,minute FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 14 AND minute <2
 6;
---FIND 
+--FIND
+SELECT account_number, amount FROM atm_transactions WHERE day = 28 AND year = 2021 AND month = 7 AND atm_location LIKE 'Leggett%' AND transaction_type = 'withdraw';
