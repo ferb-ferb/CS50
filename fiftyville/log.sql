b102be9c6a6190cb5da591355fc07ef556455f88
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-05-31 23:25:32 +0000

    Wed, May 31, 2023, 4:25 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,4 @@ SELECT * FROM interviews WHERE day = 28 AND month = 7 AND year = 2021;
 --FIND license plates that left bakery within 10 min of crime
 SELECT activity,license_plate,minute FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 14 AND minute <2
 6;
+--FIND 
