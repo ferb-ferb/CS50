11744d19ddabc044f857e9a6ab00159e798d064e
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-06-01 00:07:51 +0000

    Wed, May 31, 2023, 5:07 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,4 @@ SELECT activity,license_plate,minute FROM bakery_security_logs WHERE day = 28 AN
 --FIND
 SELECT account_number, amount FROM atm_transactions WHERE day = 28 AND year = 2021 AND month = 7 AND atm_location LIKE 'Leggett%' AND transaction_type = 'withdraw';
 SELECT caller,receiver, duration FROM phone_calls WHERE day = 28 AND year = 2021 AND month = 7 AND duration <60 ;
+SELECT name FROM people JOIN bank_accounts ON people.id = bank_accounts.person_id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE day = 28 AND year = 2021 AND month = 7 AND atm_location LIKE 'Leggett%' AND transaction_type = 'withdraw') AND phone_number IN (SELECT caller FROM phone_calls WHERE day = 28 AND year = 2021 AND month = 7 AND duration <60) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute>14 AND minute<26);
