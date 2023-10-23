aefcc9743fc7f7843236639d0a97ba8314fe41ea
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-10-23 18:56:56 +0000

    Mon, Oct 23, 2023, 11:56 AM -07:00

--- hwga/ferb.js
+++ hwga/ferb.js
@@ -1,8 +1,8 @@
+var pointer = 0;
 var number1 = 0;
-var number2 = 0;
 function texter(event){
     var trigger = event.srcElement;
-    number1 = trigger.innerHTML;
+    pointer = trigger.innerHTML;
 }
 function add(){
     alert(text);
