2c89849de9c7dae4dae815373173ec2b0eebb6ad
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-10-23 18:48:41 +0000

    Mon, Oct 23, 2023, 11:48 AM -07:00

--- hello/hello.py
+++ hello/hello.py
@@ -1,6 +1,3 @@
-def main():
-    name = input("What is your name?\n")
-    print(f"hello, {name}")
 
-
-main()
+name = input("What is your name?\n")
+print(f"hello, {name}")
---
+++ hwga/ferb.js
@@ -0,0 +1,7 @@
+var text= "";
+function texter(event){
+ text = event.innerHTML;
+}
+function print(){
+    alert(text);
+}
