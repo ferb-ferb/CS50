fc8d1c97cfea037524a2cb688e9b8eb5523f52ba
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2023-10-24 19:07:29 +0000

    Tue, Oct 24, 2023, 12:07 PM -07:00

--- hwga/ferb.js
+++ hwga/ferb.js
@@ -10,7 +10,7 @@ function plus(){
 }
 function add(){
     let z = number1 + parseInt(pointer);
-    (z);
+    document.getElementById("display").value = ("z");
     number1 = 0;
     pointer =0;
 }
