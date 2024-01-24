e199ecf68fe7f5b1d0f383f56eca37dbb4bd065f
ferb-ferb <109885162+ferb-ferb@users.noreply.github.com>
2024-01-24 06:29:27 +0000

    Tue, Jan 23, 2024, 11:29 PM -07:00

--- hwga/ferb.js
+++ hwga/ferb.js
@@ -2,10 +2,6 @@ var pointer = 0;
 var number1 = 0;
 var disp = "0";
 var number2 = 0;
-function texter(event){
-    var trigger = event.srcElement;
-    pointer = trigger.innerHTML;
-}
 function plus(){
     number1 = parseInt(disp);
     disp = 0;
